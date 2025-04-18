8976058be66cf03b0dc94c9d73710497ad31a3d3
aspart-am <florian.esparsa@gmail.com>
2025-04-18 12:16:10 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+st.set_page_config(page_title="Rémunération MSP", layout="wide")
+
+st.title("Bienvenue dans l'application de gestion de rémunération des associés")
+st.write("Utilisez le menu à gauche pour naviguer entre les pages.")
